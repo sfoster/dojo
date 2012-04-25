a001d2da37607f4dc6a3f9f1b2e6d718d839176a
Brian Arnold <brianarn@gmail.com>
2012-04-24 21:56:34 -0600

    Incorporating feedback from @snover

--- tests/router.js
+++ tests/router.js
@@ -6,9 +6,9 @@ define(["doh", "../hash", "../router"], function(doh, hash, router){
 		{
 			name: "Router API",
 			runTest: function(t) {
-				t.t(router.register, "Router doesn't have a register");
-				t.t(router.go, "Router doesn't have a go");
-				t.t(router.startup, "Router doesn't have a startup");
+				t.t(router.register, "Router has a register");
+				t.t(router.go, "Router has a go");
+				t.t(router.startup, "Router has a startup");
 			}
 		},
 		{
@@ -20,8 +20,8 @@ define(["doh", "../hash", "../router"], function(doh, hash, router){
 				});
 
 				// Make sure it looks right
-				t.t(handle.remove, "Handle doesn't have a remove");
-				t.t(handle.register, "Handle doesn't have a register");
+				t.t(handle.remove, "Handle has a remove");
+				t.t(handle.register, "Handle has a register");
 			}
 		},
 		{
@@ -45,7 +45,7 @@ define(["doh", "../hash", "../router"], function(doh, hash, router){
 			name: "Ensuring that hash changes fire routes",
 			runTest: function(t) {
 				// Due to the nature of the hashchange event,
-				// theis test is going to be async - but we have to nest it,
+				// this test is going to be async - but we have to nest it,
 				// sadly.
 
 				var d = new doh.Deferred();
